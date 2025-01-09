fd95ecb537a4dda17a63746008eefcc4bfb55393
conny <conny@musma.net>
2025-01-09 11:00:35 +0900

    setting clerk , protect route

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Page = async () => {
+  // Server Action Onboard the user
+  // 200 || 201
+
+  return (
+    <div>
+      page
+    </div>
+  )
+}
+
+export default Page
