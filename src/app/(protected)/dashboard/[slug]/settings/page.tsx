4842f57459f287ff261e045492301127fa9bdc4e
conny <conny@musma.net>
2025-01-10 14:17:30 +0900

    add settings

---
+++ src/app/(protected)/dashboard/[slug]/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Billing from '@/components/global/billing'
+
+const Page = () => {
+  return (
+    <div>
+      <Billing />
+    </div>
+  )
+}
+
+export default Page
